ed6780cd0cd7f9c966c3230966b05f61043f8a83
Andrea Frattini <60004568+AndreaFrattini@users.noreply.github.com>
2020-02-13 15:30:09 +0100

    Add files via upload

---
+++ R/getDataset.R
@@ -0,0 +1,14 @@
+#' getDataset: this function allows the user to get the dataset
+#'
+#' @return  dataset called "data"
+#' @export
+#'
+#' @examples getData()
+getDataset <- function(){
+
+  dataset <- read.csv(system.file("inst/extdata", "PopularIndicators.csv", package = "PreliminaryAnalysis"), sep = ";")
+
+  return(dataset)
+
+}
+
